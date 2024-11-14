e3d38e34e813566a28f9386f43c0b7395f624f24
Agustin de la Vega <agustin.delavega02@gmail.com>
2024-11-14 16:07:36 -0300

    Añadir página de inicio con un saludo "Hola Mundo" y estilos básicos

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  return (
+    <div className="bg-green-400 h-screen">
+      <h1 className="text-5xl text-green-50">Hola Mundo</h1>
+    </div>
+  );
+}
