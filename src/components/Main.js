00d95f82a37ba72e8e9c1a1639275de556385a35
Agustin de la Vega <agustin.delavega02@gmail.com>
2024-11-15 13:39:23 -0300

    Agregar componente Main para estructurar el contenido principal de la aplicación

---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+function Main({ children }) {
+  return (
+    <main className="flex-grow flex flex-col items-center justify-center p-4 space-y-6">
+      {children}
+    </main>
+  );
+}
+
+export default Main;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,16 @@ import { useRouter } from "next/router";
 import Header from "@/components/Header";
 import BusquedaInsumos from "@/components/BusquedaInsumos";
 import Button from "@/components/Button";
+import Main from "@/components/Main";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-green-200 via-green-300 to-green-100 flex flex-col">
       <Header />
-      <main className="flex-grow flex flex-col items-center justify-center p-4 space-y-6">
+      <Main>
         <BusquedaInsumos />
         <Button />
-      </main>
+      </Main>
     </div>
   );
 }
